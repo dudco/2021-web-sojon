0ddb0797be1d01af12ae34b013594a3b3a8dda4e
dudco <dudco1129@naver.com>
2021-06-12 03:07:42 +0900

    Typescript types

---
+++ src/type.ts
@@ -0,0 +1,94 @@
+// boolean
+let isDone: boolean = false;
+
+// null
+let n: null = null;
+
+// undefined
+let u: undefined = undefined;
+
+// number
+let decimal: number = 6;
+let hex: number = 0xf00d;
+let binary: number = 0b1010;
+let octal: number = 0o744;
+
+// string
+let color: string = "blue";
+color = 'red';
+let myName: string = `Lee`; // ES6 템플릿 문자열
+let greeting: string = `Hello, my name is ${ myName }.`; // ES6 템플릿 대입문
+
+// object
+const obj: object = {};
+
+// array
+let list1: any[] = [1, 'two', true];
+let list2: number[] = [1, 2, 3];
+let list3: Array<number> = [1, 2, 3]; // 제네릭 배열 타입
+
+// tuple : 고정된 요소수 만큼의 타입을 미리 선언후 배열을 표현
+let tuple: [string, number];
+tuple = ['hello', 10]; // OK
+tuple = [10, 'hello']; // Error
+tuple = ['hello', 10, 'world', 100]; // Error
+tuple.push(true); // Error
+
+// enum : 열거형은 숫자값 집합에 이름을 지정한 것이다.
+enum Color1 {Red, Green, Blue};
+let c1: Color1 = Color1.Green;
+
+console.log(c1); // 1
+
+enum Color2 {Red = 1, Green, Blue};
+let c2: Color2 = Color2.Green;
+
+console.log(c2); // 2
+
+enum Color3 {Red = 1, Green = 2, Blue = 4};
+let c3: Color3 = Color3.Blue;
+
+console.log(c3); // 4
+
+/*
+any: 타입 추론(type inference)할 수 없거나 타입 체크가 필요 없는 변수에 사용한다.
+var 키워드로 선언한 변수와 같이 어떤 타입의 값이라도 할당할 수 있다.
+*/
+let notSure: any = 4;
+notSure = 'maybe a string instead';
+notSure = false; // okay, definitely a boolean
+
+// void : 일반적으로 함수에서 반환값이 없을 경우 사용한다.
+function warnUser(): void {
+  console.log("This is my warning message");
+}
+
+// never : 결코 발생하지 않는 값
+function infiniteLoop(): never {
+  while (true) {}
+}
+
+function error(message: string): never {
+  throw new Error(message);
+}
+
+// 타입 추론
+
+let foo = 123; // foo는 number 타입
+foo = 'hi';    // error: Type '"hi"' is not assignable to type 'number'.
+
+let bar; // let bar: any와 동치
+
+bar = 'Hello';
+console.log(typeof bar); // string
+
+bar = true;
+console.log(typeof bar); // boolean
+
+
+// 타입 캐스팅
+const $input = document.querySelector('input["type="text"]') as HTMLInputElement;
+const val = $input.value;
+
+const $input2 = <HTMLInputElement>document.querySelector('input["type="text"]');
+const val2= $input2.value;
