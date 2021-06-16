04bd4e7bd01112433ebe20d6648b183bc5a79ebd
dudco <dudco1129@naver.com>
2021-06-16 16:32:13 +0900

    Add clock example

---
+++ hello-world-typescript/src/clock.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+
+interface ClockProps {
+}
+
+interface ClockState {
+  date: Date;
+}
+
+class Clock extends React.Component<ClockProps, ClockState> {
+  private timerID?: NodeJS.Timeout;
+  constructor(props: ClockProps) {
+    super(props);
+
+    this.state = {
+      date: new Date(),
+    }
+  }
+
+  componentDidMount() {
+    // 컴포넌트 생성될 때 호출됨
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000,
+    )
+  }
+
+  componentWillUnmount() {
+    // 컴포넌트가 없어질 때 호출됨
+    if(this.timerID) {
+      clearInterval(this.timerID);
+    }
+  }
+
+  tick() {
+    this.setState({
+      date: new Date(),
+    })
+  }
+  
+  render() {
+    return (
+      <div>
+        <h1>Hello, world</h1>
+        <h2>It is {this.state.date.toLocaleTimeString()}</h2>
+      </div>
+    )
+  }
+}
+
+export default Clock
--- hello-world-typescript/src/index.tsx
+++ hello-world-typescript/src/index.tsx
@@ -1,72 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-// interface User {
-//   firstName: string;
-//   lastName: string;
-// }
-
-// function formatName(user: User) {
-//   return user.firstName + ' ' + user.lastName;
-// }
-
-// const user: User = {
-//   firstName: "윤",
-//   lastName: "영채"
-// }
-
-// const element = <h1 tabIndex={0}>Hello {formatName(user)}</h1>
-
-// function getGreeting(user?: User) {
-//   if(user) {
-//     return <h1>Hello, {formatName(user)}</h1>
-//   }
-//   return <h1>Hello, Stranger.</h1>
-// }
-
-// function tick() {
-//   const element = (
-//     <div>
-//       <h1>Hello world</h1>
-//       <h2>It is {new Date().toLocaleTimeString()}.</h2>
-//     </div>
-//   )
-//   ReactDOM.render(
-//     element,
-//     document.getElementById('root')
-//   );
-// }
-
-// setInterval(tick, 1000);
-
-interface WelmcomeProps {
-  name: string;
-}
-
-// 함수 컴포넌트
-// function Welcome(props: WelmcomeProps) {
-//   return <h1>Hello, {props.name}</h1>
-// }
-
-// 클래스 컴포넌트
-class Welcome extends React.Component<WelmcomeProps> {
-  render() {
-    return <h1>Hello, {this.props.name}</h1>
-  }
-}
-
-function App() {
-  return (
-    <div>
-      <Welcome name="dudco1"/>
-      <Welcome name="dudco2"/>
-      <Welcome name="dudco3"/>
-      <Welcome name="dudco4"/>
-    </div>
-  )
-}
+import Clock from "./clock";
 
 ReactDOM.render(
-  <App />,
+  <Clock />,
   document.getElementById('root')
 );
